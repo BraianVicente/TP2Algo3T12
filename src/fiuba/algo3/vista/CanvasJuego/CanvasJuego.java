7c2061eca8810afe77041915973aa8baa3da73cc
soyyo5159 <soyyo5159@hotmail.com>
2016-06-22 22:29:52 -0300

    Corregí unos crashes inexplicables

--- src/fiuba/algo3/vista/CanvasJuego/CanvasJuego.java
+++ src/fiuba/algo3/vista/CanvasJuego/CanvasJuego.java
@@ -272,7 +272,7 @@ public class CanvasJuego extends Canvas implements Actualizable{
 		dibujarRangos(gc);
 	}
 	public void dibujarHalos(GraphicsContext gc){
-		if(halos != null && juego.enTablero(halos)){
+		if(halos != null && juego.enTablero(halos) && !halos.esCombinacion()){//la condición de combinación para evitar crashes
 			ArrayList<PosicionEnElPlano> haloMovimiento = juego.obtenerHaloMovimiento(halos);
 			dibujarHalo(gc,haloMovimiento,Color.YELLOW);
 			ArrayList<PosicionEnElPlano> haloAtaque = juego.obtenerHaloAtaque(halos);
